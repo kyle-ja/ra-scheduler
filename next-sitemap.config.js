069ab4c57a61ebcc549c54d85fd025e424aa9629
KyleAnthony2 <kja.51@icloud.com>
2025-06-30 16:17:35 -0400

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://schedulebypreference.com', // change this to your real site URL
+    generateRobotsTxt: true,
+  };
+  
+  
