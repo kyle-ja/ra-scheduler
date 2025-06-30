70df116da84e1f28f3eb347010b5faf6f7a08660
KyleAnthony2 <kja.51@icloud.com>
2025-06-30 15:41:58 -0400

    fix: properly nest Google verification tag inside <Head>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
-//tag added
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta name="google-site-verification" content="xewiP9Wju6-kVFZ6Yd_Em05wrLk9KlfrUNmLTwVmkS8" />
-      <Head />
+      <Head>
+        {/* Google Search Console verification tag */}
+        <meta name="google-site-verification" content="xewiP9Wju6-kVFZ6Yd_Em05wrLk9K1frUNmLTwVmkS8" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
